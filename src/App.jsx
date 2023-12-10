8d0fe2e07f88ddee76944fb34b4fa3064e020645
M world <myokokomworld@gmail.com>
2023-12-10 21:30:44 +0630

    s

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,15 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect } from "react";
 
 const App = () => {
+  const fetch = async () => {
+    await axios
+      .get("https://gg-wda3.onrender.com/")
+      .then((res) => console.log(res.data));
+  };
+  useEffect(() => {
+    fetch();
+  }, []);
   return <div>App</div>;
 };
 
